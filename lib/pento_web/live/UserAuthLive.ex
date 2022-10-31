a35e13f61ca607dcf8b2f90147f3405ef2b7b945
Abby Jones <abbyjonesdev@gmail.com>
2022-10-30 20:43:27 -0500

    a couple of log statements for debugging

--- lib/pento_web/live/UserAuthLive.ex
+++ lib/pento_web/live/UserAuthLive.ex
@@ -1,9 +1,15 @@
 defmodule PentoWeb.UserAuthLive do
   import Phoenix.LiveView
   alias Pento.Accounts
+  require Logger
 
   def on_mount(_layout, _params, %{"user_token" => user_token} = _session, socket) do
+
+    IO.puts("In on_mount")
+    IO.puts("User token: #{inspect user_token}")
     user = Accounts.get_user_by_session_token(user_token)
+    IO.puts("User: #{inspect user}")
+
     socket = socket |> assign_new(:current_user, fn -> user end)
 
     if socket.assigns.current_user do
